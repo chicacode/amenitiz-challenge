6eddd2c4e5300f7d0084ede39a4cccd58dbe99cc
chicacode <gerahernan.al@gmail.com>
2025-05-19 11:52:52 -0700

    improvement: handling errors in grandmasterPage when user is not found

--- src/pages/GrandmastersPage.tsx
+++ src/pages/GrandmastersPage.tsx
@@ -60,7 +60,7 @@ const GrandmastersPage: React.FC = () => {
   );
 
   // Handle sort change
-    const sortedUsers = [...filteredUsers].sort((a, b) => {
+  const sortedUsers = [...filteredUsers].sort((a, b) => {
     if (sortBy === 'newest') return (b.joined || 0) - (a.joined || 0);
     if (sortBy === 'oldest') return (a.joined || 0) - (b.joined || 0);
     return 0;
@@ -96,6 +96,24 @@ const GrandmastersPage: React.FC = () => {
         totalPages={totalPages}
         currentPage={currentPage}
         setCurrentPage={setCurrentPage} isLoading={isLoading} />
+
+      {searchTerm && currentUsers.length === 0 && (
+        <div className=" text-center bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+          <strong className="font-bold pr-2">Holy smokes!</strong>
+          <span className="block sm:inline">Something seriously bad happened.</span>
+          <div className="font-bold text-black text-lg mt-6">
+            No grandmasters found matching "<span className="font-semibold">{searchTerm}</span>"
+          </div>
+          <span className="absolute top-0 bottom-0 right-0 px-4 py-3" onClick={() => {
+            setSearchTerm('');
+            setCurrentPage(1);
+          }}>
+            <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+          </span>
+        </div>
+
+      )}
+
     </main>
   );
 };
